884cc62d877feaa0a7fbbd8f36d9a193d6087996
Sarosh Nasir <sarosh.nasir@gmail.com>
2018-12-06 21:46:20 +0100

    started on day 3 puzzle

--- Code3.hs
+++ Code3.hs
@@ -1,20 +1,24 @@
 module Code3 where
 
 import Data.List
-import Data.Char
-import Data.Maybe
-import System.IO
+import Data.List.Split
+import Data.Matrix
 
 ex = "#1 @ 236,827: 24x17"
 
-data Claim = Claim ID AT RECT
+ex2 = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
+
+data Claim = Claim Id At Rect
   deriving (Show)
 
-data ID = ID Int
+data Id = Id String
   deriving (Show)
 
-type AT = (Int, Int)
-type RECT = (Int, Int)
+type At = (Int, Int)
+type Rect = (Int, Int)
+type Fabric = Matrix String
+
+sizeEx = 8
 
 main :: IO ()
 main = do
@@ -25,5 +29,40 @@ main = do
   putStrLn $ show g
 
 
-toClaim :: String -> Maybe Claim
-toClaim ('#' : id : '@' : x : y : ':' : n1 : 'x' : n2) = undefined
+toClaim :: String -> Claim
+toClaim str = Claim (Id id) at re
+  where xs = words str
+        id = getID (xs !! 0)
+        at = getAts (xs !! 2)
+        re = getRects (xs !! 3)
+
+toClaims :: [String] -> [Claim]
+toClaims xs = map toClaim xs
+
+getID :: String -> String
+getID ('#':rest) = rest
+
+getAts :: String -> (Int, Int)
+getAts str = (read x, read y)
+  where clean  = take (length str - 1) str
+        (x, y) = (head (splitOn "," clean), last (splitOn "," clean)) 
+  
+getRects :: String -> (Int, Int)
+getRects str = (read x, read y)
+  where (x, y) = (head (splitOn "x" str), last (splitOn "x" str))
+
+initFabric :: Int ->  Fabric
+initFabric size = fromLists (replicate size (replicate size "."))
+
+markFabric :: Claim -> Fabric -> Fabric
+markFabric (Claim (Id id) (x,y) (i,j)) fab = undefined -- use markFabric'
+    where pos = [ (x + i', y + j') | i' <- [0..i], j' <- [0..j]]
+
+markFabric' :: String -> [(Int, Int)] -> Fabric
+markFabric' str [] fab = fab
+markFabric' str (x:xs) fab = undefined
+
+markClaims :: [Claim] -> Fabric
+markClaims xs = undefined
+
+
